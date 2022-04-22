1b45fa6b2d1d1b5286bd01c5b8245983343c7063
pprp <1115957667@qq.com>
2022-04-22 09:16:13 +0800

    fix bugs in loss and label shape in sandwich

--- train_supernet.sh
+++ train_supernet.sh
@@ -15,11 +15,11 @@ python3 train_supernet.py run \
   --max_epoch 70 \
   --batch_size 256 \
   --lr 0.001 \
-  --warmup 1 \
-  --dyna_batch_size 2 \
+  --warmup 5 \
+  --dyna_batch_size 4 \
   --pretrained checkpoints/resnet48.pdparams \
   --save_dir checkpoints/res48-ofa \
-  --log_freq 1 \
+  --log_freq 10 \
   --image_dir /data/public/imagenet2012
   # --resume checkpoints/res48-depth \
 
