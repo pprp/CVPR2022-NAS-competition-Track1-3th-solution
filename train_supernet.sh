b84e4f80dcf8546e93e8d6f13a51c5101048eb59
pprp <1115957667@qq.com>
2022-04-22 23:36:35 +0800

    update training scripts

--- train_supernet.sh
+++ train_supernet.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #SBATCH -N 1     # 需要使用的节点数
 #SBATCH -J supnet      # 作业名字
-#SBATCH --gres=gpu:4   # 需要使用的卡数
+#SBATCH --gres=gpu:8   # 需要使用的卡数
 
 #此处可填写加载程序运行所需环境（根据软件需求，可使用 module load export 等方式加载）
 module load anaconda/2020.11 cuda/10.2 \
  cudnn/7.6.5.32_cuda10.2 nccl/2.9.6-1_cuda10.2
 source activate pp
+
+# 将数据加载到内存中
+mkdir -p /dev/shm/imagenet2012
+tar -kxf /data/public/imagenet2012/train.tar -C /dev/shm/imagenet2012 & tar -kxf /data/public/imagenet2012/val.tar -C /dev/shm/imagenet2012 
 
 # 此处可填写运行程序的命令
 
@@ -20,7 +24,7 @@ python3 train_supernet.py run \
   --pretrained checkpoints/resnet48.pdparams \
   --save_dir checkpoints/res48-ofa \
   --log_freq 10 \
-  --image_dir /data/public/imagenet2012
-  # --resume checkpoints/res48-depth \
+  --image_dir /dev/shm/imagenet2012 \
+  --resume checkpoints/res48-ofa \
 
 
